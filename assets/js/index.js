a4fc6ba0e776f2a56432e668f06be9ef4aa7f8c7
ElGansoDorado <yakovchikdenis@gmail.com>
2023-09-14 21:59:11 +0300

    fix: close

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,9 +1,29 @@
 //open modal
 document.getElementById("button-open-modal").addEventListener("click", function() {
-    document.getElementById("my-modal").classList.add("open")
-})
+    document.getElementById("my-modal").classList.add("open");
+    document.getElementById("body").classList.add("lock");
+});
 
 //close modal
 document.getElementById("button-close-modal").addEventListener("click", function() {
-    document.getElementById("my-modal").classList.remove("open")
-})
+    document.getElementById("my-modal").classList.remove("open");
+    document.getElementById("body").classList.remove("lock");
+});
+
+//close enter esc
+window.addEventListener('keydown', (e) => {
+    if (e.key === "Escape") {
+        document.getElementById("my-modal").classList.remove("open")
+        document.getElementById("body").classList.remove("lock");
+    }
+});
+
+//
+document.querySelector("#my-modal .modal-box").addEventListener('click', event => {
+    event._isClickWithInModal = true;
+});
+document.getElementById("my-modal").addEventListener('click', event => {
+    if (event._isClickWithInModal) return;
+    event.currentTarget.classList.remove('open');
+    document.getElementById("body").classList.remove("lock");
+});
