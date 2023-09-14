8c56985a17613c2a1ec61791b2c44e47639b9fce
ElGansoDorado <yakovchikdenis@gmail.com>
2023-09-14 21:41:43 +0300

    feat: add function for close modal window

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,9 @@
+//open modal
 document.getElementById("button-open-modal").addEventListener("click", function() {
     document.getElementById("my-modal").classList.add("open")
+})
+
+//close modal
+document.getElementById("button-close-modal").addEventListener("click", function() {
+    document.getElementById("my-modal").classList.remove("open")
 })
